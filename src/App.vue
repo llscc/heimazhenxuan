b4126bcb098f7e0b6f0bcc84f211fa6fcbecc63a
Snare Li <lsclcl@sina.com>
2023-05-23 15:51:58 +0800

    feat: new

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <h1>i am app</h1>
-    <el-button type="primary" size="default" :icon="Plus">按钮</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
-// 引入图标
-import { Plus } from '@element-plus/icons-vue'
+
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,28 @@
 import { createApp } from 'vue'
 
-import App from './App.vue'
+import App from '@/App.vue'
 // 引入element
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // 配置element国际化
 //@ts-ignore
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// svg
+import 'virtual:svg-icons-register'
+
+// 引入自定义插件对象
+import gloablComponent from './components/index';
 
 const app = createApp(App)
 
 app.use(ElementPlus, {
   locale: zhCn,
 })
+
+app.use(gloablComponent);
+// 引入全局样式
+import '@/styles/index.scss'
+
+
+
 app.mount('#app')
