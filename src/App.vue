c5abec69fcdf79014df7e226707fa8a7143eca77
Snare Li <lsclcl@sina.com>
2023-05-23 13:53:03 +0800

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>i am app</h1>
+    <el-button type="primary" size="default" :icon="Plus">按钮</el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+// 引入图标
+import {Plus} from '@element-plus/icons-vue'
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+// 引入element
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 配置element国际化
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+const app = createApp(App)
+
+app.use(ElementPlus, {
+    locale: zhCn,
+  })
+app.mount('#app')
+
